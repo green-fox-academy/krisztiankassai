3f1f60f1deded032272b437e4e9c7ab5c9924708
KrisztianKassai <krisztiankassai21@gmail.com>
2018-05-10 13:26:39 +0200

    HelloOthers exercise done

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Krisz");
+        System.out.println("Hello Lili");
+        System.out.println("Hello Akos");
+    }
+}
