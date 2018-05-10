f9d58fd8faf4ed9b8726cd8ba79700ee9683662e
KrisztianKassai <krisztiankassai21@gmail.com>
2018-05-10 13:10:14 +0200

    HelloMe exercise done

---
+++ week-01/day-4/src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        String textToPrint = "Hello Seagal";
+
+        boolean isitTrue = true;
+
+        System.out.println(textToPrint);
+
+    }
+
+}
