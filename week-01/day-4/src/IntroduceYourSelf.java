20057ba25fa6eb0ca2a3b7c57f87fefd6c4d3d4e
KrisztianKassai <krisztiankassai21@gmail.com>
2018-05-10 13:32:24 +0200

    IntroduceYourSelf exercise is done

---
+++ week-01/day-4/src/IntroduceYourSelf.java
@@ -0,0 +1,7 @@
+public class IntroduceYourSelf {
+    public static void main(String[] args) {
+        System.out.println("Krisztian Kassai");
+        System.out.println("29");
+        System.out.println("1.86");
+    }
+}
