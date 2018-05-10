92e733de58843e10718310775d75cbba7b9c61ef
KrisztianKassai <krisztiankassai21@gmail.com>
2018-05-10 16:23:51 +0200

    DefineBasicInfo exercise done

---
+++ week-01/day-4/src/DefineBasicInfo.java
@@ -0,0 +1,14 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String myname = "Krisztian Kassai";
+        int  myage = 29;
+        double myheight = 1.86;
+        boolean amimarried = false;
+
+        System.out.println("My name is: " + myname);
+        System.out.println("My age is: " + myage);
+        System.out.println("My height is: " + myheight);
+        System.out.println("Am I married? " + amimarried);
+
+    }
+}
