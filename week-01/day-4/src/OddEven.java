3c522e643bfed1e6ddd60ee16c90c243ed9d21c6
KrisztianKassai <krisztiankassai21@gmail.com>
2018-05-11 13:03:05 +0200

    OddEven is done

---
+++ week-01/day-4/src/OddEven.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give me a number ");
+
+        int number = scanner.nextInt();
+
+        if (number % 2 ==0){
+            System.out.println("The number is even");
+        } else {
+            System.out.println("Number is odd");
+        }
+
+        // Write a program that reads a number from the standard input,
+// Then prints "Odd" if the number is odd, or "Even" if it is even.
+    }
+}
