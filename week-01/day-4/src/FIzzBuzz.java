4eacdfc83817b6bbdea6d34d8a75c188b98b9f40
KrisztianKassai <krisztiankassai21@gmail.com>
2018-05-13 21:10:31 +0200

    FizzBuzz exercise done

---
+++ week-01/day-4/src/FIzzBuzz.java
@@ -0,0 +1,23 @@
+public class FIzzBuzz {
+    public static void main(String[] args) {
+
+
+
+        for (int i = 1; i < 101; i++) {
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("fizzbuzz");
+            } else if (i % 3 == 0){
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+        // Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the number
+// and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”.
+    }
+}
