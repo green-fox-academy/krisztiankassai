fdf673e77fd4ddce54aeb26943735439e50fadcb
KrisztianKassai <krisztiankassai21@gmail.com>
2018-05-14 17:19:48 +0200

    DiagonalMatrix and Printall exercises done

---
+++ week-02/day1/src/DiagonalMatrix.java
@@ -0,0 +1,19 @@
+public class DiagonalMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = new int[4][4];
+
+        for (int i = 0; i < matrix.length ; i++) {
+            System.out.println("");
+            for (int j = 0; j < matrix.length ; j++) {
+                if (i == j){
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+
+            }
+
+        }
+
+    }
+}
