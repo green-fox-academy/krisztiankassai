9c6fb59eb791427b5732bf1afcd42f80a0a77e51
KrisztianKassai <krisztiankassai21@gmail.com>
2018-05-14 13:10:42 +0200

    Drawpyramid and GuessTHeNumber exercise done

---
+++ week-02/day1/src/Doubling.java
@@ -0,0 +1,10 @@
+public class Doubling {
+    public static void main(String[] args) {
+        int basenum = 123;
+
+        System.out.println(doubling(basenum));
+    }
+    public static int doubling(int number){
+        return number*2;
+    }
+}
