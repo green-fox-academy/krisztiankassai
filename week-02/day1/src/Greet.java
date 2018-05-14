2819c8ec7753964b495c2ba16b687f99092da472
KrisztianKassai <krisztiankassai21@gmail.com>
2018-05-14 13:32:10 +0200

    Greet exercise done

---
+++ week-02/day1/src/Greet.java
@@ -0,0 +1,15 @@
+public class Greet {
+    public static void main(String[] args) {
+        String al = "GreenFox";
+
+        greet();
+        System.out.print(al);
+
+    }
+    public static void greet(){
+
+        System.out.print("Greetings dear, ");
+
+    }
+
+}
