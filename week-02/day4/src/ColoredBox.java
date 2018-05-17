8d1389e8e9b9382a620b6fcc0e2850b9a7acf38e
KrisztianKassai <krisztiankassai21@gmail.com>
2018-05-17 13:11:22 +0200

    ColoredBox, Diagonals, Gotocenter exercises done

---
+++ week-02/day4/src/ColoredBox.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+
+public class ColoredBox {
+    public static void mainDraw(Graphics graphics) {
+        // Draw a box that has different colored lines on each edge.
+
+        graphics.setColor(Color.green);
+        graphics.drawLine(0,0, 50,0);
+        graphics.setColor(Color.blue);
+        graphics.drawLine(50,0,50,50);
+        graphics.setColor(Color.red);
+        graphics.drawLine(50,50,0,50);
+        graphics.setColor(Color.orange);
+        graphics.drawLine(0,50,0,0);
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
---
+++ week-02/day4/src/GoToCenter.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class GoToCenter {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 2 parameters:
+        // The x and y coordinates of the line's starting point
+        // and draws a line from that point to the center of the canvas.
+        // Draw 3 lines with that function. Use loop for that.
+        int x = 0 ;
+        int y = 0 ;
+            drawLine(x, y, graphics);
+    }
+    public static void drawLine(int x, int y, Graphics graphics){
+        for (int i = 0; i < 3 ; i++) {
+            graphics.drawLine((int) (Math.random() * 320),(int) (Math.random() * 320),160,160);
+        }
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
+
