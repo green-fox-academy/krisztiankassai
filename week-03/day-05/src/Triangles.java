954e98404299292cd7a680a4efa4a0ba6d215a97
KrisztianKassai <krisztiankassai21@gmail.com>
2018-05-25 13:53:55 +0200

    Small triangles added

--- week-03/day-05/src/Triangles.java
+++ week-03/day-05/src/Triangles.java
@@ -7,16 +7,21 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class Triangles {
     public static void mainDraw(Graphics graphics) {
 
-        pyramiddrawer(graphics, 5,5,300);
+        pyramidDrawer(graphics, 5,5,500);
+        smallPyramidDrawer(graphics, 5,5,500);
 
     }
-    public static void pyramiddrawer(Graphics graphics, int x, int y, int size){
+    public static void pyramidDrawer(Graphics graphics, int x, int y, int size){
         int[] xx = {x, x + size, x + size / 2};
         int[] yy = {y, y, (int)(Math.sqrt(3) / 2 * size)};
         int n = 3;
         graphics.drawPolygon(xx,yy,n);
     }
-    
+    public static void smallPyramidDrawer(Graphics graphics, int x, int y, int size){
+        pyramidDrawer(graphics,x ,y , size / 2 );
+        pyramidDrawer(graphics,x + size / 2,y, size / 2 );
+        pyramidDrawer(graphics, x + size / 4,(int)(Math.sqrt(3) / 2 * size / 2),size / 2);
+    }
 
     static int WIDTH = 320;
     static int HEIGHT = 343;
