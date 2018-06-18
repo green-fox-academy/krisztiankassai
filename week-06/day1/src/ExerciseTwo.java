1a25eedd06099ecff6b4179b232cf5c62c0c735e
KrisztianKassai <krisztiankassai21@gmail.com>
2018-06-18 13:09:36 +0200

    Exercise two done

--- week-06/day1/src/ExerciseTwo.java
+++ week-06/day1/src/ExerciseTwo.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ExerciseTwo {
+
+  public static void main(String[] args) {
+
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+    numbers.stream().filter(x -> x % 2 != 0 && x > 0).forEach(System.out::println);
+    System.out.println(getEvenAverage(numbers));
+
+
+  }
+  public static double getEvenAverage(List<Integer> numbers){
+    return numbers.stream()
+            .filter(x -> x % 2 != 0 && x > 0)
+            .mapToInt(x -> x)
+            .average()
+            .getAsDouble();
+  }
 
 }
