da934baf7e97ccd3d065db94609496d89e5fd835
KrisztianKassai <krisztiankassai21@gmail.com>
2018-06-20 13:20:09 +0200

    Html and CSS for fancy table exercise added

--- week-06/day1/src/ExerciseFive.java
+++ week-06/day1/src/ExerciseFive.java
@@ -2,6 +2,7 @@ import com.sun.xml.internal.fastinfoset.util.CharArray;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,4 +12,14 @@ public class ExerciseFive {
 
     String uppercase = "thisStringHasUpperCaseCharacters";
 
-}}
+    System.out.println(getUpperCaseCharacters(uppercase));
+    }
+
+
+  public static List getUpperCaseCharacters(String example){
+    return Stream.of(example)
+            .sorted()
+            .collect(Collectors.toList());
+
+  }
+}
