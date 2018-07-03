7ee2d44a441e965f619fa3ed7391b533405dc1ac
KrisztianKassai <krisztiankassai21@gmail.com>
2018-07-03 13:18:13 +0200

    Functions added

--- week-07/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/Services/SkeletonService.java
+++ week-07/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/Services/SkeletonService.java
@@ -1,8 +1,11 @@
 package com.greenfoxacademy.programmerfoxclub.Services;
 import com.greenfoxacademy.programmerfoxclub.Model.Skeleton;
 
+import java.util.ArrayList;
+
 public interface SkeletonService {
 
    void addSkeleton(Skeleton skeleton);
 
+   Skeleton getSkeleton(String name);
 }
---
+++ week-07/programmerfoxclub/src/main/java/com/greenfoxacademy/programmerfoxclub/Services/TrickService.java
@@ -0,0 +1,4 @@
+package com.greenfoxacademy.programmerfoxclub.Services;
+
+public class TrickService {
+}
